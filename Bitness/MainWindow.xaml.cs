2f9b63d7ce72928bbddaefb959c6d9d9a3953eca
Mike Nolan <me@nolski.rocks>
2015-01-01 01:01:49 -0500

    Video drawn to screen

--- Bitness/MainWindow.xaml.cs
+++ Bitness/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
 
 namespace Bitness
 {
@@ -20,9 +21,70 @@ namespace Bitness
     /// </summary>
     public partial class MainWindow : Window
     {
+        private KinectSensor sensor;
+        private byte[] colorPixels;
+        private WriteableBitmap colorBitmap;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            foreach (KinectSensor sensor in KinectSensor.KinectSensors)
+            {
+                if (sensor.Status == KinectStatus.Connected)
+                {
+                    // Get the first sensor
+                    this.sensor = sensor;
+                    break;
+                }
+            }
+
+            if (this.sensor != null)
+            {
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth,
+                    this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                this.Camera.Source = this.colorBitmap;
+                this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch(System.IO.IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    this.sensor = null;
+                }
+            }
+        }
+
+        private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame == null)
+                    return;
+
+                colorFrame.CopyPixelDataTo(this.colorPixels);
+                this.colorBitmap.WritePixels(
+                    new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                    this.colorPixels, this.colorBitmap.PixelWidth * sizeof(Int32), 0);
+            }
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+
     }
 }
