9f6b1d43df719221b5e9e607a27af69667cca849
Mike Nolan <me@nolski.rocks>
2015-03-24 15:31:53 -0400

    outline of actions working

---
+++ Bitness/Action.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitness
+{
+    interface Action
+    {
+
+        bool InProgress { get; }
+        int Reps { get; }
+
+        WatchedJoint[] WatchedJoints
+        {
+            get;
+        }
+
+        string Update (IReadOnlyDictionary<JointType, Joint> joints);
+    }
+}
---
+++ Bitness/WatchedJoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitness
+{
+    class WatchedJoint
+    {
+        private JointType jointType;
+        private int direction;
+
+        public JointType Joint
+        {
+            get
+            {
+                return jointType;
+            }
+        }
+
+        public WatchedJoint (JointType jointType, int direction)
+        {
+            this.jointType = jointType;
+            this.direction = direction;
+        }
+    }
+}
